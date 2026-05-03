8e8db47ab37f99635663f8b7187018982d4058bc
Kristinathomask1506 <kristinathomask1506@gmail.com>
2026-05-03 20:27:48 +0530

    Remove 'Certifications' from Navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,6 @@ const navLinks = [
   { name: 'Skills', href: '#skills' },
   { name: 'Projects', href: '#projects' },
   { name: 'Education', href: '#education' },
-  { name: 'Certifications', href: '#certifications' },
   { name: 'Contact', href: '#contact' },
 ];
 
